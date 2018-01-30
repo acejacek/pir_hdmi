858991b1f0407bf18fc3470052589e9683796ec0
aceJacek <acejacek@gmail.com>
2018-01-30 21:57:02 +0100

    improving --debug handling

--- pirhdmi.py
+++ pirhdmi.py
@@ -5,38 +5,51 @@ import sys, os, getopt, time
 from subprocess import call
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)        #number by phisical pin 
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setmode(GPIO.BOARD)        # number by phisical pin 
+GPIO.setup(11, GPIO.IN)         # Read output from PIR motion sensor
 
 def readPir(debug):
     status = "off"         # [FIXME] this should be guessed by system
+
+    #f = open(os.devnull, 'w')  # redirect stdout to /dev/null
+    #sys.stdout = f
+
     while True:
         i=GPIO.input(11)
-        if i==0:                 #When output from motion sensor is LOW
-            if status == "on":   #if monitor is ON, tunr it off
+        if i==0:                 # When output from motion sensor is LOW
+            if status == "on":   # if monitor is ON, tunr it off
                 if debug:
-                    print "No intruders",i
+                    print "No intruders, monitor goes OFF."
                 call(["/opt/vc/bin/tvservice", "--off"])
                 time.sleep(1)
                 status = "off"
-        elif i==1:               #When output from motion sensor is HIGH
+        elif i==1:               # When output from motion sensor is HIGH
             if status == "off":  # if monitor is OFF, switch it off
                 if debug:
-                    print "Intruder detected",i
+                    print "Intruder detected, turning monitor ON."
                 call(["/opt/vc/bin/tvservice", "--preferred"])
-                time.sleep(30)     # monitor on for at least 30 seconds
+                time.sleep(30)     # keep monitor on for at least 30 seconds
                 status = "on"
+
+def helper():
+    sys.exit('Usage: sudo %s [--debug]' % sys.argv[0])
 
 def main(params):
     debug = False
-    if params == "--debug":
-        debug = True
+    if len(sys.argv) > 1:
+        if sys.argv[1] in ["--debug","-v","--verbose"]:
+            print "Run in debug mode"
+            debug = True
+        else:
+            helper()
+    
     readPir(debug)
 
 if __name__ == "__main__":
 
     if os.geteuid() != 0:
-        sys.exit('Root access needed. Usage: sudo %s [--debug]' % sys.argv[0])
+        print "Root access needed."
+        helper()
     else:
-        readPir(sys.argv[1:])
+        main(sys.argv[1])
 
