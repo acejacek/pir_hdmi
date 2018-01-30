504c82fcd339a0bcce239d56cd68b510466b73d7
aceJacek <acejacek@gmail.com>
2018-01-30 22:35:49 +0100

    implement functions

--- pirhdmi.py
+++ pirhdmi.py
@@ -8,36 +8,46 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)        # number by phisical pin 
 GPIO.setup(11, GPIO.IN)         # Read output from PIR motion sensor
 
-def readPir(debug=False):
-    status = "off"         # [FIXME] this should be guessed by system
+def turn_hdmi(mode="on"):
+
+    if mode == "off":
+        call(["/opt/vc/bin/tvservice", "--off"])
+        time.sleep(1)
+    else:
+        call(["/opt/vc/bin/tvservice", "--preferred"])
+        time.sleep(30)     # keep monitor on for at least 30 seconds
+    
+    return mode
+
+def readPir(debug = False):
+
+    if debug:
+        print "Starting with monitor ON."
+    status = turn_hdmi("on")         # by default, turn on HDMI
 
     #f = open(os.devnull, 'w')  # redirect stdout to /dev/null
     #sys.stdout = f
 
     while True:
-        i=GPIO.input(11)
-        if i==0:                 # When output from motion sensor is LOW
-            if status == "on":   # if monitor is ON, tunr it off
+        i = GPIO.input(11)
+        if i == 0:                 # When output from motion sensor is LOW
+            if status == "on":   # if monitor is ON, turn it off
                 if debug:
                     print "No intruders, monitor goes OFF."
-                call(["/opt/vc/bin/tvservice", "--off"])
-                time.sleep(1)
-                status = "off"
-        elif i==1:               # When output from motion sensor is HIGH
+                status = turn_hdmi("off")
+        elif i == 1:               # When output from motion sensor is HIGH
             if status == "off":  # if monitor is OFF, switch it off
                 if debug:
                     print "Intruder detected, turning monitor ON."
-                call(["/opt/vc/bin/tvservice", "--preferred"])
-                time.sleep(30)     # keep monitor on for at least 30 seconds
-                status = "on"
+                status = turn_hdmi("on")
 
 def helper():
-    sys.exit('Usage: sudo %s [--debug]' % sys.argv[0])
+    sys.exit('Usage: %s [--debug]' % sys.argv[0])
 
 if __name__ == "__main__":
 
     if os.geteuid() != 0:
-        print "Root access needed."
+        print "Root access needed. Try sudo."
         helper()
     elif len(sys.argv) > 1:
         if sys.argv[1] in ["--debug","-v","--verbose"]:
