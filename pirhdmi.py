c6b3741db9c3bd65d4668913e34534283a045f7c
aceJacek <acejacek@gmail.com>
2018-01-30 20:53:39 +0100

    Renowed version of script

--- pirhdmi.py
+++ pirhdmi.py
@@ -7,13 +7,30 @@ from subprocess import call
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)        #number by phisical pin 
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
-while True:
-       i=GPIO.input(11)
-       if i==0:                 #When output from motion sensor is LOW
-             # print "No intruders",i
-             call(["/opt/vc/bin/tvservice", "--off"])
-             time.sleep(1)
-       elif i==1:               #When output from motion sensor is HIGH
-             # print "Intruder detected",i
-             call(["/opt/vc/bin/tvservice", "--preferred"])
-             time.sleep(30)     # monitor on for at least 30 seconds
+
+def readPir(debug):
+    status = "off"         # [FIXME] this should be guessed by system
+    while True:
+        i=GPIO.input(11)
+        if i==0:                 #When output from motion sensor is LOW
+            if status == "on":   #if monitor is ON, tunr it off
+                if debug:
+                    print "No intruders",i
+                call(["/opt/vc/bin/tvservice", "--off"])
+                time.sleep(1)
+                status = "off"
+        elif i==1:               #When output from motion sensor is HIGH
+            if status == "off":  # if monitor is OFF, switch it off
+                if debug:
+                    print "Intruder detected",i
+                call(["/opt/vc/bin/tvservice", "--preferred"])
+                time.sleep(30)     # monitor on for at least 30 seconds
+                status = "on"
+
+def main(argv):
+    debug = True
+    readPir(debug)
+
+if __name__ == "__main__":
+    readPir(sys.argv[1:])
+
