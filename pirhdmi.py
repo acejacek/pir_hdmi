11fc15b46d9a8db5802d579c03012400b640b58d
aceJacek <acejacek@gmail.com>
2018-01-31 19:12:57 +0100

    change from sensor polling to edge detect

--- pirhdmi.py
+++ pirhdmi.py
@@ -4,44 +4,40 @@ import RPi.GPIO as GPIO
 import sys, os, getopt, time
 from subprocess import call
 
+PIR_PIN = 11
+debug = False
+
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)        # number by phisical pin 
-GPIO.setup(11, GPIO.IN)         # Read output from PIR motion sensor
+GPIO.setup(PIR_PIN, GPIO.IN)         # Read output from PIR motion sensor
 
-def turn_hdmi(mode="on"):
+def turn_hdmi(channel):
 
-    if mode == "off":
-        call(["/opt/vc/bin/tvservice", "--off"])
-        time.sleep(1)
-    else:
+    if GPIO.input(channel):          # GPIO in state HIGH
         call(["/opt/vc/bin/tvservice", "--preferred"])
-        time.sleep(30)     # keep monitor on for at least 30 seconds
-    
-    return mode
+        if debug:
+            print "Intruder detected, turning monitor ON."
+    else:                       # GPIO in state LOW
+        call(["/opt/vc/bin/tvservice", "--off"])
+        if debug:
+            print "No intruders, monitor goes OFF."
 
-def readPir(debug = False):
+
+def readPir():
 
 #    f = open(os.devnull, 'w')  # redirect stdout to /dev/null
 #    sys.stdout = f
 
     if debug:
-        print "Starting with monitor ON."
-    status = turn_hdmi("on")         # by default, turn on HDMI
+        print "Set initial state of HDMI port based on PIR input."
+    #call(["/opt/vc/bin/tvservice", "--off"])
+    turn_hdmi(PIR_PIN)
 
+    GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=turn_hdmi, bouncetime=200)
 
     while True:
-        i = GPIO.input(11)
-        if i == 0:                 # When output from motion sensor is LOW
-            if status == "on":   # if monitor is ON, turn it off
-                if debug:
-                    print "No intruders, monitor goes OFF."
-                status = turn_hdmi("off")
-        elif i == 1:               # When output from motion sensor is HIGH
-            if status == "off":  # if monitor is OFF, switch it off
-                if debug:
-                    print "Intruder detected, turning monitor ON."
-                status = turn_hdmi("on")
-        time.sleep(0.5)
+        time.sleep(1e6)
 
 def helper():
     sys.exit('Usage: %s [--debug]' % sys.argv[0])
@@ -54,11 +50,11 @@ if __name__ == "__main__":
     elif len(sys.argv) > 1:
         if sys.argv[1] in ["--debug","-v","--verbose"]:
             print "Run in debug mode"
-            readPir(True)
+            debug=True
         else:
             helper()
-    else:
-        readPir()
+    
+    readPir()
 
    
 
